6478a6068851240a4e96768b72b3a519ac16b5cb
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-29 11:28:39 +0200

    Initial implementation for installing FileZilla

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,17 @@
-class filezilla {
-  anchor { 'Hello_World': }
+# Public: Install OmniDiskSweeper to /Applications
+#
+# Examples
+#
+#  include filezilla
+#  class { 'filezilla':
+#    version => '3.7.3'
+#  }
+#
+class filezilla($version='3.7.3') {
+
+  package { "FileZilla-${version}":
+    provider => 'compressed_app',
+    source   => "http://heanet.dl.sourceforge.net/project/filezilla/FileZilla_Client/${version}/FileZilla_${version}_i686-apple-darwin9.app.tar.bz2",
+  }
+
 }
